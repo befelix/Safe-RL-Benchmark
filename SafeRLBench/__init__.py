d44d76a9b2781efa0aec7ce8aa87230c4a38d268
Felix Berkenkamp <fberkenkamp@gmail.com>
2016-11-17 14:34:10 +0100

    Add testing code

--- SafeRLBench/__init__.py
+++ SafeRLBench/__init__.py
@@ -1,3 +1,12 @@
-from .policygradient import *
+from __future__ import absolute_import
 
+from .policygradient import *
+from . import envs
+from . import tools
+
+# Add everython to all
 __all__ = [s for s in dir() if not s.startswith('_')]
+
+# Import test after __all__ (no documentation)
+from numpy.testing import Tester
+test = Tester().test
