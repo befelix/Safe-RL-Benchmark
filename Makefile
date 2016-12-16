c887706344dfa3da5270d5051a2a46289c704b48
Nicolas Ochsner <ochsnern@student.ethz.ch>
2016-12-16 13:45:28 +0100

    Changes to test scripts.

---
+++ Makefile
@@ -0,0 +1,34 @@
+module="SafeRLBench"
+
+GREEN=\033[0;32m
+NC=\033[0m
+
+# Flake 8 ignore errors
+flakeignore='E402,W503'
+
+# Pydocstyle ignore errors
+pydocignore='D105'
+
+style:
+	@echo "${GREEN}Running style tests:${NC}"
+	@flake8 ${module} --exclude test*.py,__init__.py --show-source
+	@flake8 ${module} --filename=__init__.py,test*.py --ignore=F --show-source
+
+docstyle:
+	@echo "${GREEN}Testing docstring conventions:${NC}"
+	@pydocstyle ${module} --match='(?!__init__).*\.py' 2>&1 | grep -v "WARNING: __all__"
+
+unittests2:
+	@echo "${GREEN}Running unit tests for 2.7:${NC}"
+	@nosetests-2.7 -v --with-doctest --with-coverage --cover-erase --cover-package=${module} ${module}
+
+unittests3:
+	@echo "${GREEN}Running unit tests for 3.5:${NC}"
+	@nosetests-3.5 -v --with-doctest --with-coverage --cover-erase --cover-package=${module} ${module}
+
+unittests: unittests2 unittests3
+
+test: style docstyle unittests
+
+clean:
+	find . -type f -name '*.pyc' -exec rm -f {} ';'
--- test_code.sh
+++ test_code.sh
@@ -20,20 +20,26 @@ cd $(get_script_dir)
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
 # Run style tests
-echo -e "${GREEN}Running style tests.${NC}"
-flake8 $module --exclude test*.py,__init__.py --ignore=E402,W503 --show-source
+echo -e "${GREEN}${BOLD}Running style tests:${NC}"
+flake8 $module --exclude test*.py,__init__.py --show-source
 
 # Ignore import errors for __init__ and tests
-flake8 $module --filename=__init__.py,test*.py --ignore=F,E402,W503 --show-source
+flake8 $module --filename=__init__.py,test*.py --ignore=F --show-source
 
-echo -e "${GREEN}Testing docstring conventions.${NC}"
+echo -e "${GREEN}${BOLD}Testing docstring conventions:${NC}"
 # Test docstring conventions
 pydocstyle $module --match='(?!__init__).*\.py' 2>&1 | grep -v "WARNING: __all__"
 
-# Run unit tests
-echo -e "${GREEN}Running unit tests.${NC}"
-nosetests -v --with-doctest --with-coverage --cover-erase --cover-package=$module $module
+# Run unit tests python 2.7
+echo -e "${GREEN}${BOLD}Running unit tests for 2.7:${NC}"
+nosetests-2.7 -v --with-doctest --with-coverage --cover-erase --cover-package=$module $module
+
+echo -e "${GREEN}${BOLD}Running unit tests for 3.5:${NC}"
+nosetests-3.5 -v --with-doctest --with-coverage --cover-erase --cover-package=$module $module
 
 # Export html
 coverage html
