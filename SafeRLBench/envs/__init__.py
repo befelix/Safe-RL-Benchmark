847343b66eb2a16b10b511d72f6740212b699d47
Nicolas Ochsner <ochsnern@student.ethz.ch>
2017-02-01 15:25:39 +0100

    [FIX] Import error on master branch tests.

--- SafeRLBench/envs/__init__.py
+++ SafeRLBench/envs/__init__.py
@@ -2,6 +2,5 @@ from __future__ import absolute_import
 
 from .general_mountaincar import GeneralMountainCar
 from .linear_car import LinearCar
-from .gym_wrap import GymWrap
 
 __all__ = ['GeneralMountainCar', 'LinearCar', 'GymWrap']
