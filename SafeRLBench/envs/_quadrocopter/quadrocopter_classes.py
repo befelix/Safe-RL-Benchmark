9af971b69b3c7f92cb8d6ab999e22e276027619d
Nicolas Ochsner <ochsnern@student.ethz.ch>
2017-03-19 20:10:39 +0100

    [ADD] Quadrocopter environment. Reward still WIP.

---
+++ SafeRLBench/envs/_quadrocopter/quadrocopter_classes.py
@@ -0,0 +1,160 @@
+# flake8: noqa
+
+"""
+quadrocopter_classes.py
+
+Written By: Adrian Esser and David Wu
+
+Changes:
+- Aug 2015 - Vectorized quadrotor state, moved state conversions here
+
+This file contains all classes for the quadrocopter simulation!
+
+This class defines an object for the state of the drone.
+The state contains the position, velocity, and acceleration information,
+the rotation matrix (which implies pitch, roll, and yaw), and the angular
+velocity information.
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+
+from .transformations import (quaternion_from_euler, euler_from_matrix,
+                              euler_matrix)
+
+
+__all__ = ['State', 'Parameters', 'StateVector']
+
+
+class StateVector(object):
+    """Class to provide same StateVector object as used in ROS."""
+
+    def __init__(self):
+        super(StateVector, self).__init__()
+        self.pos = np.zeros(3)
+        self.vel = np.zeros(3)
+        self.acc = np.zeros(3)
+        self.euler = np.zeros(3)
+        self.omega_g = np.zeros(3)
+        self.omega_b = np.zeros(3)
+        self.quat = np.zeros(4)
+        self.quat[3] = 1
+
+
+class State:
+
+    def __init__(self):
+
+        self.R = np.eye(3)
+        self.pos = np.zeros(3)
+        self.vel = np.zeros(3)
+        self.acc = np.zeros(3)
+        self.omega = np.zeros(3)
+
+    @property
+    def quaternion(self):
+        """Rotation quaternion corresponding to R."""
+        return quaternion_from_euler(*self.rpy)
+
+    @property
+    def rpy(self):
+        """Roll, pitch, yaw corresponding to R."""
+        return np.array(euler_from_matrix(self.R))
+
+    @property
+    def state_vector(self):
+        """Return the state as a StateVector."""
+        state = StateVector()
+        state.pos[:] = self.pos
+        state.vel[:] = self.vel
+        state.acc[:] = self.acc
+        state.quat[:] = self.quaternion
+        state.euler[:] = self.rpy
+        state.omega_b[:] = self.omega
+        state.omega_g[:] = self.R.dot(self.omega)
+        return state
+
+    def rpy_to_R(self, rpy):
+        return euler_matrix(*rpy)[:3, :3]
+
+
+class Parameters:
+    """Parameters for quadrotor the define the physics."""
+
+    def __init__(self):
+
+        # m, mass of vehicle (kg)
+        self.m = 1.477
+        # g, mass normalized gravitational force (m/s^2)
+        self.g = 9.8
+        # L, vehicle arm length (m)
+        self.L = 0.18
+        # K, motor constant, determined experimentally
+        self.K = 0.26
+        # Ix, inertia around the body's x-axis (kg-m^2)
+        self.Ix = 0.01152
+        # Iy, inertia around the body's y-axis (kg-m^2)
+        self.Iy = 0.01152
+        # Iz, inertia around the body's z-axis (kg-m^2)
+        self.Iz = 0.0218
+        # fmin, mass normalized minimum rotor force (m/s^2)
+        self.fmin = 0.17
+        # fmax, mass normalized maximum rotor force (m/s^2)
+        self.fmax = 6.0
+        # vmax, maximum quadrotor velocity (m/s)
+        self.vmax = 2.0
+        # eta, damping ratio
+        self.eta = 0.707
+        # tau_z, time constant for vertical direction
+        self.tau_z = 1.0
+        # tau_Iz, integral time constant for vertical direction
+        self.tau_Iz = 0.05
+        # tau_yaw, time constant for yaw rate
+        self.tau_yaw = 0.55
+        # tau_Iyaw, integral time constant for yaw rate
+        self.tau_Iyaw = 0.01
+        # eta_y, damping ratio
+        self.eta_y = 0.707
+        # tau_y, time constant for x and y direction
+        self.tau_y = 1.7
+        # tau_Iu, integral time constant for x and y dir.
+        self.tau_Iu = 2.5
+        # tau_p, time constant for roll rate
+        self.tau_p = 0.18
+        # tau_q, time constant for pitch rate
+        self.tau_q = 0.18
+        # tau_r, time constant for yaw rate
+        self.tau_r = 0.1
+        # tau_rp, time constant
+        self.tau_rp = 0.18
+        # tau_f, time constant for force integration
+        self.tau_f = 0.1
+
+        # Air drag factor in body x direction [dimensionless]
+        self.CD_bx = 0.55
+        # Air drag factor in body y direction [dimensionless]
+        self.CD_by = 1.25
+        # Air drag factor in body z direction [dimensionless]
+        self.CD_bz = 0.3
+
+        # Air drag factor in body x direction [-]
+        self.CD_bx = 0.35
+        # Air drag factor in body y direction [-]
+        self.CD_by = 1.25
+        # Air drag factor in body z direction [-]
+        self.CD_bz = 0.3
+
+        # Delay in the signal being sent from quad to computer (us)
+        self.incoming_delay = 0.0
+        # Delay in signal being sent from computer to quad (us)
+        self.outgoing_delay = 100000.0
+        # Update rate of inner loop (us)
+        self.inner_loop_cycle = 8000.0
+        # Update rate of outer loop (us)
+        self.outer_loop_cycle = 15000.0
+
+        # Takeoff height (m)
+        self.takeoff_height = 1.0
+        # Takeoff speed (m/s)
+        self.takeoff_speed = 0.25
---
+++ SafeRLBench/envs/quadrocopter.py
@@ -0,0 +1,309 @@
+"""Quadrocopter environment wrapper."""
+
+from __future__ import division, print_function, absolute_import
+
+from SafeRLBench import EnvironmentBase
+
+from ._quadrocopter import QuadrotorDynamics
+from ._quadrocopter import StateVector
+
+from functools import partial
+
+from six import string_types
+
+import numpy as np
+from numpy import array
+from numpy import pi, cos, sin
+
+# Available reference functions.
+REFERENCE_TYPES = ['circle', 'stationary', 'oscillate']
+
+
+class Quadrocopter(EnvironmentBase):
+    """Quadrocopter simulation.
+
+    Attributes
+    ----------
+    model : model object
+        Object simulating the quadrotor dynamics.
+    horizon : int
+        Number of iterations for the main simulation
+    pre_sim_horizon : int
+        Number of iterations for the pre-simulation.
+    """
+
+    def __init__(self, state_space, action_space,
+                 init_pos=None, init_vel=None, num_sec=9,
+                 num_init_sec=4, ref='circle', period=1 / 70.,
+                 seed=None):
+        """Quadrocopter initialization.
+
+        Parameters
+        ----------
+        init_pos : array-like
+            Initial position of the quadrocopter. Default: None ; which will
+            set init_pos to [1, 0, 0].
+        init_vel : array-like
+            Initial velocity of the quadrocopter. Default: None ; which will
+            set init_vel to [0, pi / 2, 0]
+        num_sec :
+        num_init_sec :
+        ref : string or reference object
+            Name of the reference. Currently supported are 'circle',
+            'stationary' or 'oscillate'.
+        period : float
+
+        seed : int
+
+        """
+        if seed is not None:
+            np.random.seed = seed
+            self._seed = seed
+
+        # initial position
+        if init_pos is None:
+            init_pos = array([cos(0), sin(0), 0.])
+
+        if len(init_pos) != 3:
+            raise ValueError("init_pos with invalid length %d.", init_pos)
+
+        # initial velocity
+        if init_vel is None:
+            init_vel = array([-pi / 2. * sin(0), pi / 2. * cos(0), 0.])
+
+        if len(init_vel) != 3:
+            raise ValueError("init_vel with invalid length %d.", init_vel)
+
+        # initialize model
+        self.model = QuadrotorDynamics(init_pos, init_vel)
+
+        if isinstance(ref, string_types):
+            self.reference = Reference(ref, period)
+
+        self.horizon = int(1. / period) * num_sec
+        self.pre_sim_horizon = int(1. / period) * num_init_sec
+
+        self.period = period
+
+        self._trajectory = np.atleast_2d(np.zeros(3))
+        self._time = []
+        self._step = 0
+
+    def _update(self, action):
+        assert self.action_space.contains(action), "Invalid action."
+
+        self.model.update_position(action)
+
+        state_vec = self.state_vec
+
+        self._step += 1
+        self._time.append(self._step * self.period)
+        self._trajectory = np.vstack((self._trajectory, self.state.pos))
+
+        state = np.hstack((state_vec.pos,
+                           state_vec.vel,
+                           state_vec.euler,
+                           state_vec.omega_b))
+
+        # TODO: Implement reward computation.
+        reward = 0
+
+        return action, state, reward
+
+    def _reset(self):
+        self._trajectory = np.atleast_2d(np.zeros(3))
+        self._time = []
+        self._step = 0
+
+    @property
+    def seed(self):
+        """Seed."""
+        return self._seed
+
+    @seed.setter
+    def seed(self, value):
+        np.random.seed(value)
+        self._seed = value
+
+    @property
+    def state_vec(self):
+        """Provide access to state_vector."""
+        # this whole state vector implementation is annoyingly inefficient.
+        return self.model.state.state_vector
+
+    @state_vec.setter
+    def state_vec(self, state_vec):
+        self.model.state.state_vector = state_vec
+
+
+class Reference(object):
+    """Reference object for quadrocopter environment."""
+
+    def __init__(self, name='circle', period=1. / 70, keep_record=True,
+                 **kwargs):
+        """Initialize Reference.
+
+        Parameters
+        ----------
+        name : str
+            The name of the reference function.
+        period : float
+            The time step that the simulation takes every iteration.
+        keep_record : bool
+            Whether the history of the reference object should be saved.
+        **kwargs : dict
+        """
+        # name type checking.
+        if not isinstance(name, string_types):
+            raise ValueError('Invalid type for argument name.')
+        if name not in REFERENCE_TYPES:
+            raise ValueError(name + ' is not a valid reference.')
+
+        self._name = name
+        self._period = period
+        self._iter = 0
+        self._reference_function = self._reference_chooser(**kwargs)
+        self.keep_record = keep_record
+        if keep_record:
+            self._record = []
+
+    @property
+    def name(self):
+        """Return the type of reference function."""
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value not in REFERENCE_TYPES:
+            raise ValueError(value + ' is not a valid reference.')
+
+        self.reset()
+
+        self._name = value
+        self._reference_function = self._ref_chooser()
+
+    @property
+    def record(self):
+        """Return the reference record of the simulation."""
+        if self.keep_record:
+            return np.atleast_2d(self._record)
+        else:
+            print("WARNING: Reference record has not been saved.")
+
+    def reset(self):
+        """Reset internal state."""
+        self._iter = 0
+        if self.keep_record:
+            self._record = []
+
+    def compute(self, state, finished=False):
+        """Compute the state of the reference object."""
+        time = self._iter * self._period
+        ref = self._reference_function(state, time, finished)
+        self._iter += 1
+        if self.keep_record:
+            ref_value = np.hstack((ref.pos, ref.vel, ref.euler, ref.omega_b))
+            self._update_record(ref_value)
+        return ref
+
+    def _update_record(self, ref_value):
+        self._record.append(ref_value)
+        assert self._iter == len(self._record)
+
+    def _reference_chooser(self, **kwargs):
+        # CIRCLE
+        if self._name == 'circle':
+            if kwargs.get('speed', False):
+                speed = kwargs['speed']
+            else:
+                speed = np.pi / 2.
+            if kwargs.get('initial_angle', False):
+                init_angle = kwargs['initial_angle']
+            else:
+                init_angle = 0.
+            if kwargs.get('radius', False):
+                radius = kwargs['radius']
+            else:
+                radius = 1.
+            if kwargs.get('z_vel', False):
+                z_vel = kwargs['z_vel']
+            else:
+                z_vel = 0.
+            return partial(_circle_reference,
+                           speed=speed,
+                           init_angle=init_angle,
+                           radius=radius,
+                           z_vel=z_vel)
+        # STATIONARY
+        elif self._name == 'stationary':
+            if kwargs.get('position', False):
+                position = kwargs['position']
+            else:
+                position = [1., 0., 0.]
+            return partial(_stationary_reference,
+                           position=position)
+        # OSCILLATE
+        elif self._name == 'oscillate':
+            if kwargs.get('x_velocity', False):
+                x_vel = kwargs['x_velocity']
+            else:
+                x_vel = 0.5
+            if kwargs.get('omega', False):
+                omega = kwargs['omega']
+            else:
+                omega = 1.
+            if kwargs.get('radius', False):
+                radius = kwargs['radius']
+            else:
+                radius = 0.5
+            return partial(_oscillate_reference,
+                           x_vel=x_vel,
+                           omega=omega,
+                           radius=radius)
+
+
+# private circle reference function
+def _circle_reference(state,
+                      time,
+                      finished,
+                      radius=None,
+                      speed=None,
+                      init_angle=None,
+                      z_vel=None):
+    ref = StateVector()
+    angle = init_angle + speed / radius * time
+    ref.pos[:] = [radius * np.cos(angle), radius * np.sin(angle), z_vel * time]
+    ref.vel[:] = [-speed * np.sin(angle), speed * np.cos(angle), z_vel]
+    ref.euler[2] = np.pi + np.arctan2(state.pos[1], state.pos[0])
+    # reference.omega_b[2] = speed / radius
+    return ref
+
+
+# private stationary reference function
+def _stationary_reference(state,
+                          time,
+                          finished,
+                          position=None):
+    ref = StateVector()
+    ref.pos[0] = position[0]
+    ref.pos[1] = position[1]
+    ref.pos[2] = position[2]
+    return ref
+
+
+# private oscillation reference function
+def _oscillate_reference(state,
+                         time,
+                         finished,
+                         x_vel=None,
+                         omega=None,
+                         radius=None):
+    ref = StateVector()
+    angle = omega * time
+    ref.pos[0] = x_vel * time
+    ref.pos[1] = radius * np.sin(angle)
+    ref.pos[2] = 0.
+    ref.vel[0] = x_vel
+    ref.vel[1] = radius * omega * np.cos(angle)
+    ref.vel[2] = 0.
+    return ref
