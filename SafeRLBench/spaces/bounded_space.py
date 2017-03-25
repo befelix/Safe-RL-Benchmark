35e8bb0c58cced353a51428daa8450c6d6ad322c
Nicolas Ochsner <ochsnern@student.ethz.ch>
2017-03-25 20:22:01 +0100

    [DOC] Better BoundedSpace documentation.

--- SafeRLBench/spaces/bounded_space.py
+++ SafeRLBench/spaces/bounded_space.py
@@ -6,23 +6,54 @@ from numpy.random import rand
 
 
 class BoundedSpace(Space):
-    """
-    Bounded subspace of R^n.
+    """Bounded subspace of R^n.
 
-    Usage:
-    BoundedSpace(np.array(-1,-2), np.array(1,0))
-    or
-    BoundedSpace(-1, 1)
+    Attributes
+    ----------
+    lower : array-like
+        Lower bound
+    upper : array-like
+        Upper bound
+
+    Examples
+    --------
+    The `BoundedSpace` class can be instatiated in two ways. If you have
+    individual bounds for each dimension, then you can directly pass the
+    `lower` or `upper` bound as an array-like.
+
+    >>> BoundedSpace(np.array(-1,-2), np.array(1,0))
+
+    In this case the shape argument will be ignored. If you want to create a
+    however shaped box, where all the bounds are the same, then, you may pass
+    a lower and an upper bound as a scalar and make sure that you specify the
+    shape.
+
+    >>> BoundedSpace(-1, 1, shape=(2,))
     """
 
     def __init__(self, lower, upper, shape=None):
-        """Initialize BoundedSpace."""
+        """Initialize BoundedSpace.
+
+        Parameters
+        ----------
+        lower : array-like
+            Lower bound of the space. Either an array or an integer.
+            Must agree with the input of the upper bound.
+        upper : array-like
+            Upper bound of the space. Either an array or an integer. Must
+            agree with the input of the lower bound.
+        shape : integer
+            Shape of the bounds. Input will be ignored, if the bounds are non
+            scalar, if they are scalar, it must be set.
+        """
         if (np.isscalar(lower) and np.isscalar(upper)):
+            assert shape is not None, "Shape must be set, if bounds are scalar"
             self.lower = np.zeros(shape) + lower
             self.upper = np.zeros(shape) + upper
         else:
-            self.lower = lower
-            self.upper = upper
+            self.lower = np.array(lower)
+            self.upper = np.array(upper)
+            assert self.lower.shape == self.upper.shape, "Shapes do not agree."
 
         self._dim = None
 
