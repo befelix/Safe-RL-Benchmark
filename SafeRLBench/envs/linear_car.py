1b35022fd9234a63edf82d89620c2683af07e32c
Nicolas Ochsner <ochsnern@student.ethz.ch>
2017-04-01 13:09:33 +0200

    [DOC] Updated LinearCar docs.

--- SafeRLBench/envs/linear_car.py
+++ SafeRLBench/envs/linear_car.py
@@ -1,40 +1,69 @@
 """Linear Car."""
 import numpy as np
-from numpy import copy
+from numpy import copy, array
 from numpy.linalg import norm
 
 from SafeRLBench import EnvironmentBase
 from SafeRLBench.spaces import RdSpace, BoundedSpace
 
 
-# TODO: LinearCar: update docs, add examples
+# TODO: LinearCar: add examples
 class LinearCar(EnvironmentBase):
-    """Implementation of LinearCar Environment."""
+    """Implementation of LinearCar Environment.
 
-    def __init__(self,
-                 state_space=RdSpace((2, 1)),
-                 action_space=BoundedSpace(-1, 1, (1,)),
-                 state=np.array([[0.], [0.]]), goal=np.array([[1.], [0.]]),
+    This is a very simple environment implementing a car in an arbitrarily
+    dimensioned space. By default it will just be one dimensional, which
+    results in a two dimensional state space, that is, (pos, vel), and
+    accordingly in a one dimensional bounded action space, that is, the
+    acceleration.
+
+    Attributes
+    ----------
+    state : ndarray
+        Current state of the LinearCar.
+    initial_state : ndarray
+        Initial state of the LinearCar.
+    goal : ndarray
+        Goal state.
+    eps : float
+        Margin for completion. If 0, the goal is to stabilize at the goal
+        completly.
+    step : float
+        Update step.
+    state_space : Space object
+        State space as deduced from the state.
+    action_space : Space object
+        Action space as deduced from the state.
+    """
+
+    def __init__(self, state=array([[0.], [0.]]), goal=array([[1.], [0.]]),
                  step=0.01, eps=0, horizon=100):
         """
-        Initialize EnvironmentBase parameters and specific parameters.
+        Initialize LinearCar.
 
-        Baseclass Parameters as in base.py.
-
-        Attributes
+        Parameters
         ----------
-        state: array-like
-            Element of state_space. Specifies initial state.
-        goal: array-like
-            Element of state_space. Specifies goal state.
-            The goal state should contain zero velocity, anything else does
-            not make sense.
-        step: float
-        eps: float
+        state : ndarray
+            Initial state of the LinearCar. The state and action space will be
+            deduced from this. The shape needs to be (2, d) for d > 0.
+        goal : ndarray
+            Goal state of the LinearCar. The shape should comply to the shape
+            of the initial state.
+            In case the velocity is non-zero, eps should be strictly greater
+            than zero, since there is no way for the system to stabilize in
+            the goal state anyway.
+        eps : float
             Reward at which we want to abort. If zero we do not abort at all.
+        step : float
+            Update step.
         """
-        # Initialize EnivronmentBase Parameters
-        super(LinearCar, self).__init__(state_space, action_space, horizon)
+        assert state.shape[0] == 2, 'Invalid shape of the initial state.'
+        assert state.shape == goal.shape, 'State and goal shape have to agree.'
+
+        # Initialize EnivronmentBase attributes
+        self.horizon = horizon
+        self.state_space = RdSpace(state.shape)
+        self.action_space = BoundedSpace(-1, 1, shape=(state.shape[1],))
 
         # Initialize State
         self.initial_state = state
